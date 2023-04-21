8e836508547ef0b0c36142c88fa62a2c98d95fa7
Victor Leina Ngeny <victorleinangeny@gmail.com>
2023-04-21 23:11:59 +0300

    lab attempted

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,19 @@
-import React from "react";
+//how to add state:
+
+//1. import the useState hook
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
-}
+  //st up the initial state
+  const [isOn, setIsOn] = useState(false);
+  function handleClick(){
+    setIsOn((isOn)=> !isOn);
+  }
+const color = isOn? "red":"white";
 
+  return (
+  <button style={{background: color}}onClick={handleClick}> 
+  {isOn? "ON" : "OFF"} </button>
+  );
+}
 export default Toggle;
